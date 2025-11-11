54962ceea4ade7a56910a8092b55c805ae98c351
Urvi <urvijoshi811@gmail.com>
2025-11-11 11:11:30 +0000

    Added distributor with broker

---
+++ Dgol-skeleton-broker/gol/stubs/stubs.go
@@ -0,0 +1,36 @@
+package stubs
+
+import "uk.ac.bris.cs/gameoflife/util"
+
+/*
+var ExecGolMethod = "Connection.GolMethod"
+var AliveCellsMethod = "Connection.AliveCellsMethod"
+*/
+var SaveWorld = "ConnectionBroker.SaveWorld"
+var TerminateWorld = "ConnectionBroker.TerminateWorld"
+var PauseWorld = "ConnectionBroker.PauseWorld"
+var TerminateClient = "ConnectionBroker.TerminateClient"
+
+// var NewMethod = "ConnectionBroker.ExecMethod"
+var AliveCellsBrokers = "ConnectionBroker.AliveCellsMethod"
+var NextState = "Connection.NextStateInit"
+var ParallelGolMethod = "ConnectionBroker.ParallelGolMethod"
+var AliveCells = "Connection.AliveCells"
+var TerminateWorker = "Connection.TerminateWorker"
+
+type Response struct {
+	FinalWorld  [][]byte
+	AliveCells  []util.Cell
+	CurrentTurn int
+}
+
+type Request struct {
+	StartWorld  [][]byte
+	Turns       int
+	Height      int
+	Width       int
+	StartY      int
+	EndY        int
+	CurrentTurn int
+	Threads     int
+}
