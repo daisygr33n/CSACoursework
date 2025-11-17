a04e0f19abc1eebcc7ad48e078b0fbe364a19eae
Urvi <urvijoshi811@gmail.com>
2025-11-17 14:17:36 +0000

    Added fault tolerance files

---
+++ Dgol-skeleton-broker-fault tolerance/gol/broker/broker.go
@@ -0,0 +1,444 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"net"
+	"time"
+
+	//"flag"
+	"fmt"
+	//"log"
+	//"net"
+	"net/rpc"
+	"sync"
+
+	"uk.ac.bris.cs/gameoflife/gol/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+type ConnectionBroker struct {
+	aliveCells  []util.Cell //slice of aliveCells that can be shared by GoLmethod and AliveCellsreport
+	currentTurn int         //current turn of gol
+	mu          sync.Mutex  //mutex lock to protect critical section (reading and writing to alivecells)
+}
+
+var (
+	globalWorld  [][]byte
+	terminate    = false
+	paused       = false
+	shutDown     = make(chan bool)
+	golFinished  = make(chan bool)
+	aliveWorkers = make([]bool, 4)
+	mu           sync.Mutex
+)
+
+//var shutDown = make(chan bool)
+//var golFinished = make(chan bool)
+
+func (c *ConnectionBroker) AliveCellsMethod(req stubs.Request, res *stubs.Response) (err error) {
+	c.mu.Lock()
+	res.AliveCells = c.aliveCells
+	res.CurrentTurn = c.currentTurn
+	c.mu.Unlock()
+	return
+}
+
+func (c *ConnectionBroker) SaveWorld(request stubs.Request, res *stubs.Response) (err error) {
+	c.mu.Lock()
+	mu.Lock()
+	res.FinalWorld = globalWorld
+	res.AliveCells = c.aliveCells
+	res.CurrentTurn = c.currentTurn
+	c.mu.Unlock()
+	mu.Unlock()
+	return
+}
+
+func (c *ConnectionBroker) TerminateWorld(request stubs.Request, res *stubs.Response) (err error) {
+	mu.Lock()
+	terminate = true
+	c.mu.Lock()
+	res.FinalWorld = globalWorld
+	res.AliveCells = c.aliveCells
+	res.CurrentTurn = c.currentTurn
+	c.mu.Unlock()
+	mu.Unlock()
+	return
+}
+
+func (c *ConnectionBroker) PauseWorld(request stubs.Request, res *stubs.Response) (err error) {
+	mu.Lock()
+
+	if paused {
+		paused = false
+
+	} else {
+		paused = true
+	}
+
+	c.mu.Lock()
+	res.FinalWorld = globalWorld
+	res.AliveCells = c.aliveCells
+	res.CurrentTurn = c.currentTurn
+	c.mu.Unlock()
+	mu.Unlock()
+	return
+}
+
+func (c *ConnectionBroker) TerminateClient(request stubs.Request, res *stubs.Response) (err error) {
+	mu.Lock()
+	terminate = true
+	c.mu.Lock()
+	res.FinalWorld = globalWorld
+	res.AliveCells = c.aliveCells
+	res.CurrentTurn = c.currentTurn
+	c.mu.Unlock()
+	mu.Unlock()
+
+	client, err := rpc.Dial("tcp", "localhost:8040")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func(client *rpc.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("error closing client: ", err)
+		}
+	}(client)
+
+	var req stubs.Request
+	var resTemp stubs.Response
+	client.Go(stubs.TerminateWorker, req, &resTemp, nil)
+
+	go func() {
+		<-golFinished
+		shutDown <- true
+	}()
+
+	return
+}
+
+func HeartbeatMonitor(workerIndex int, client *rpc.Client) {
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			var resHeartbeat stubs.Heartbeat
+
+			err := client.Call(stubs.HeartbeatWorker, stubs.Request{}, &resHeartbeat)
+			if err != nil {
+				fmt.Println(err)
+				//mu.Lock()
+				//aliveWorkers[workerIndex] = false
+				//mu.Unlock()
+			} else {
+				mu.Lock()
+				aliveWorkers[workerIndex] = resHeartbeat.Alive
+				mu.Unlock()
+			}
+			if !resHeartbeat.Alive {
+				return
+			}
+		}
+	}()
+}
+
+func replaceWorker(workerIndex int) *rpc.Client {
+	ports := []string{"8050", "8060", "8070", "8080"}
+	addr := "localhost:" + ports[workerIndex]
+
+	newWorker, err := rpc.Dial("tcp", addr)
+	if err != nil {
+		fmt.Println(err, "error redialing")
+	}
+	mu.Lock()
+	aliveWorkers[workerIndex] = true
+	mu.Unlock()
+	HeartbeatMonitor(workerIndex, newWorker)
+	fmt.Println("worker", workerIndex, "redialed")
+	return newWorker
+}
+
+func (c *ConnectionBroker) ParallelGolMethod(req stubs.Request, res *stubs.Response) (err error) {
+	client, err := rpc.Dial("tcp", "localhost:8040")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func(client *rpc.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("error closing client: ", err)
+		}
+	}(client)
+
+	c.mu.Lock()
+	c.currentTurn = 0
+	c.aliveCells = make([]util.Cell, 0)
+	c.mu.Unlock()
+
+	height := req.Height
+	width := req.Width
+	threads := 4
+	rows := height / 4
+
+	responses := make([]*stubs.Response, 4)
+	done := make(chan *rpc.Call, 4)
+
+	currentWorld := req.StartWorld
+	mu.Lock()
+	globalWorld = currentWorld
+	mu.Unlock()
+
+	var aliveCells []util.Cell
+	client.Call(stubs.AliveCells, req, &res)
+	aliveCells = res.AliveCells
+
+	c.mu.Lock()
+	c.aliveCells = aliveCells
+	c.mu.Unlock()
+
+	clients := make([]*rpc.Client, 4)
+
+	/*for i := 0; i < threads; i++ {
+		client, err := rpc.Dial("tcp", "8040")
+		if err != nil {
+			fmt.Println(err)
+		}
+		clients[i] = client
+	}*/
+
+	client0, err := rpc.Dial("tcp", "localhost:8050")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		clients[0] = client0
+		mu.Lock()
+		aliveWorkers[0] = true
+		mu.Unlock()
+	}
+	HeartbeatMonitor(0, client0)
+
+	client1, err := rpc.Dial("tcp", "localhost:8060")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		clients[1] = client1
+		mu.Lock()
+		aliveWorkers[1] = true
+		mu.Unlock()
+	}
+	HeartbeatMonitor(1, client1)
+
+	client2, err := rpc.Dial("tcp", "localhost:8070")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		clients[2] = client2
+		mu.Lock()
+		aliveWorkers[2] = true
+		mu.Unlock()
+	}
+	HeartbeatMonitor(2, client2)
+
+	client3, err := rpc.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		clients[3] = client3
+		mu.Lock()
+		aliveWorkers[3] = true
+		mu.Unlock()
+	}
+	HeartbeatMonitor(3, client3)
+
+loopTurns:
+	for round := 0; round < req.Turns; round++ {
+
+		mu.Lock()
+		if terminate {
+			mu.Unlock()
+			break loopTurns
+		}
+		mu.Unlock()
+
+		mu.Lock()
+		localPaused := paused
+		mu.Unlock()
+
+		if localPaused {
+		loopPaused:
+			for {
+				mu.Lock()
+				if !paused {
+					mu.Unlock()
+					break loopPaused
+				}
+				mu.Unlock()
+			}
+		}
+
+		calls := make([]*rpc.Call, threads)
+		//fmt.Println(currentWorld)
+
+		for i := 0; i < 4; i++ {
+
+			startY := i * rows
+			topHalo := startY
+			if startY > 0 {
+				topHalo--
+			}
+			endY := (i + 1) * rows
+
+			if i == threads-1 {
+				endY = height - 1
+			}
+			//fmt.Println(startY, "startY", topHalo, "topHalo")
+			slice := make([][]byte, rows+2)
+			if startY == 0 {
+				slice[0] = currentWorld[height-1]
+				copy(slice[1:], currentWorld[topHalo:endY+1])
+			} else if endY == height-1 {
+				slice[rows+1] = currentWorld[0]
+				copy(slice[0:rows+1], currentWorld[topHalo:endY+1])
+			} else {
+				slice = currentWorld[topHalo : endY+1]
+			}
+			//fmt.Println(topHalo, endY, len(slice))
+			newReq := stubs.Request{
+				StartWorld:  slice,
+				StartY:      startY,
+				EndY:        endY,
+				Height:      len(slice),
+				Width:       width,
+				Turns:       req.Turns,
+				CurrentTurn: round,
+				Threads:     threads,
+				WorldHeight: height,
+				WorkerIndex: i,
+			}
+
+			mu.Lock()
+			alive := aliveWorkers[i]
+			mu.Unlock()
+
+			if !alive {
+				fmt.Println("worker", i, "is dead")
+				time.Sleep(5 * time.Second)
+				newClient := replaceWorker(i)
+				clients[i] = newClient
+			}
+
+			//var resTemp stubs.Response
+			var resTemp = new(stubs.Response)
+			responses[i] = resTemp
+			calls[i] = clients[i].Go(stubs.NextState, newReq, &resTemp, done)
+
+		}
+
+		errorFree := true
+		for i := 0; i < 4; i++ {
+			<-calls[i].Done
+
+			if calls[i].Error != nil {
+				errorFree = false
+				fmt.Println("worker", i, "failed mid-turn")
+				fmt.Println("failed at turn", round)
+				mu.Lock()
+				aliveWorkers[i] = false
+				mu.Unlock()
+			}
+		}
+
+		//fmt.Println("trying turn", round)
+		if errorFree {
+			//fmt.Println("executed", round)
+			aliveCells = nil
+			var nextWorld [][]byte
+			for i := 0; i < 4; i++ {
+				nextWorld = append(nextWorld, responses[i].FinalWorld...)
+				aliveCells = append(aliveCells, responses[i].AliveCells...)
+			}
+			c.mu.Lock()
+			c.aliveCells = aliveCells
+			c.currentTurn++
+			c.mu.Unlock()
+
+			currentWorld = nextWorld
+		} else {
+			round--
+		}
+
+		mu.Lock()
+		globalWorld = currentWorld
+		mu.Unlock()
+
+	}
+	c.mu.Lock()
+	res.FinalWorld = currentWorld
+	res.CurrentTurn = c.currentTurn
+	res.AliveCells = aliveCells
+	c.mu.Unlock()
+
+	mu.Lock()
+	terminate = false
+	paused = false
+	mu.Unlock()
+
+	select {
+	case <-golFinished:
+	default:
+		close(golFinished)
+	}
+
+	return
+}
+
+func main() {
+	err := rpc.Register(&ConnectionBroker{}) //registers Connection struct methods for RPC
+	if err != nil {
+		fmt.Println("rpc register error:", err)
+	}
+	pAddr := flag.String("port", "8030", "Port to listen on") //specify a port
+	flag.Parse()
+	listener, err := net.Listen("tcp", ":"+*pAddr) //listen on tcp port
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer listener.Close()
+	fmt.Printf("Listening on port %s\n", *pAddr)
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
+					return
+				}
+				fmt.Println("Error", err)
+				continue
+			}
+			go rpc.ServeConn(conn)
+		}
+	}()
+
+	/*defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println("listener close error:", err)
+		}
+	}(listener)
+	fmt.Printf("Listening on port %s\n", *pAddr)
+	rpc.Accept(listener) //blocking call constantly accepting incoming connections*/
+
+	<-shutDown
+
+	fmt.Println("Shutting down...")
+	err1 := listener.Close()
+	if err1 != nil {
+		return
+	}
+	fmt.Println("Goodbye!")
+}
